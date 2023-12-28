f232eb8d2c60f8378b804cd5226da043fe6d5616
Avinda Alamsyah <it4@transcon-id.com>
2023-12-28 10:25:48 +0700

    chore(init): init new project

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text>Anothe elemen here!!</Text>
+      </View>
+      <Text>Hello World!!!!</Text>
+      <Button title='Tap Me...!'></Button>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
